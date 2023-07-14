d5f0262ef26e0d8b236d467286319406c23ba19a
stefangrevelink <stefan@allesonline.nl>
2023-07-14 15:24:43 +0200

    Use prefix for dashboard redirect

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,6 @@ class DashboardController extends Controller
 {
     public function getIndex(): \Illuminate\Http\RedirectResponse
     {
-        return redirect('/admin/content');
+        return redirect(config('component-skins.prefix') . 'content');
     }
 }
