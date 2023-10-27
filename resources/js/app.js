834423ba0f512628226db8cef652eb36a382b739
Wilco Kuijper <wilco@allesonline.nl>
2023-10-27 17:27:35 +0200

    Update swiper block

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,6 @@
 require("./bootstrap");
 
-import $ from "jquery";
-
 import Swiper from "swiper";
 import "swiper/css/bundle";
 
-$(".swiper-container").each(function () {
-    let swiper = $(this).attr("id", Math.floor(Math.random() * 10) + 1);
-    new Swiper(swiper.get());
-});
+new Swiper('.swiper');
