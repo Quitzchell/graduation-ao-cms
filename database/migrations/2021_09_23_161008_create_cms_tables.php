acd974cc4650710f31bba9039f28ce1c76b07dc7
Wilco Kuijper <wilco@allesonline.nl>
2024-01-17 09:09:05 +0100

    Update default migration for support with Digitalocean Managed MySQL

--- database/migrations/2021_09_23_161008_create_cms_tables.php
+++ database/migrations/2021_09_23_161008_create_cms_tables.php
@@ -17,7 +17,7 @@ class CreateCmsTables extends Migration
             $table->id();
             $table->integer('order');
             $table->string('uri')->nullable();
-            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable()->index();
             $table->unsignedBigInteger('linkable_id')->nullable();
             $table->string('linkable_type')->nullable();
             $table->boolean('locked')->default(false);
@@ -28,7 +28,7 @@ class CreateCmsTables extends Migration
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('template_name');
+            $table->string('template_name')->index();
             $table->string('tag')->nullable();
             $table->boolean('locked')->default(false);
             $table->timestamps();
@@ -36,6 +36,7 @@ class CreateCmsTables extends Migration
         });
 
         Schema::create('cms_loader', function (Blueprint $table) {
+            $table->id();
             $table->string('owner_type');
             $table->unsignedBigInteger('owner_id');
             $table->unsignedBigInteger('content_id');
@@ -47,28 +48,24 @@ class CreateCmsTables extends Migration
         Schema::create('cms_content', function (Blueprint $table) {
             $table->id();
             $table->integer('order')->default(0);
-            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable()->index();
             $table->string('tag', 55);
             // Language filter
             $table->string('language', 20)->nullable();
             // Loading group that are loaded separate from the owner such a page titles etc
             $table->string('group', 55)->nullable();
-
-            $table->index('parent_id');
         });
 
         Schema::create('cms_content_string', function (Blueprint $table) {
-            $table->unsignedBigInteger('content_id');
+            $table->id();
+            $table->unsignedBigInteger('content_id')->index();
             $table->string('value');
-
-            $table->index('content_id');
         });
 
         Schema::create('cms_content_text', function (Blueprint $table) {
-            $table->unsignedBigInteger('content_id');
+            $table->id();
+            $table->unsignedBigInteger('content_id')->index();
             $table->text('value');
-
-            $table->index('content_id');
         });
     }
 
