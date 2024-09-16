5710461409cc877246a32a503f224f5383ed0d9a
Wilco Kuijper <wilco@allesonline.nl>
2024-09-16 15:21:35 +0200

    Update Inertia version

--- src/laravel/app/Http/Middleware/HandleInertiaRequests.php
+++ src/laravel/app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -35,8 +36,10 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $ziggy = new Ziggy(url: $request->url());
+
         return array_merge(parent::share($request), [
-            //
+             'ziggy' => $ziggy->toArray(),
         ]);
     }
 }
