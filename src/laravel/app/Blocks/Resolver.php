abfcb0ff4cb319c322bfe997ee78902ba08b4d90
Wilco Kuijper <wilco@allesonline.nl>
2024-09-16 15:22:22 +0200

    Update block Resolver

--- src/laravel/app/Blocks/Resolver.php
+++ src/laravel/app/Blocks/Resolver.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Blocks;
 
 use AO\Component\Models\CmsContent;
+use AO\Component\Models\PredefinedBlock;
 use App\Blocks\DTO\BlockData;
 use Illuminate\Contracts\Container\BindingResolutionException;
 
@@ -20,23 +21,64 @@ class Resolver
         $blocks = [];
 
         foreach ($objects as $block) {
-            $blockClass = $this->blockClass($block);
+            if ($block->value === '_predefined_block') {
+                $blocks = [...$blocks, ...$this->resolvePredefinedBlock($block)];
+            } else {
+                $blockClass = $this->blockClass($block);
 
-            if (class_exists($blockClass)) {
-                $blocks[] = new BlockData(
-                    $this->blockName($block),
-                    'block-' . $block->getKey(),
-                    app()->make($blockClass, ['block' => $block]),
-                );
+                if (class_exists($blockClass)) {
+                    $blocks[] = new BlockData(
+                        $this->blockName($block),
+                        'block-' . $block->getKey(),
+                        app()->make($blockClass, ['block' => $block]),
+                    );
+                }
             }
         }
 
         return $blocks;
     }
 
+    /**
+     * @return array<int, BlockData>
+     * @throws BindingResolutionException
+     */
+    protected function resolvePredefinedBlock(CmsContent $block): array
+    {
+        $id = $block->content('id');
+
+        if (!$id) {
+            \Log::debug("incorrectly formatted predefined block, missing id field");
+            return [];
+        }
+
+        /** @var PredefinedBlock $block */
+        $predefinedBlock = PredefinedBlock::find($id);
+
+        if (!$predefinedBlock) {
+            \Log::debug("could not find predefined block: $id");
+            return [];
+        }
+
+        return $this->resolve($predefinedBlock->objects('blocks'));
+    }
+
     protected function blockName(CmsContent $block): string
     {
-        return str_replace(' ', '\\', ucwords(str_replace('/', ' ', $block->toArray()['_template'])));
+        $segments = array_map(
+            static function ($segment) {
+                $parts = preg_split(
+                    pattern: '/([-_])/',
+                    subject: $segment,
+                    flags: PREG_SPLIT_NO_EMPTY
+                );
+
+                return implode(array_map('ucfirst', $parts));
+            },
+            explode('/', $block->toArray()['_template'])
+        );
+
+        return implode('\\', $segments);
     }
 
     protected function blockClass(CmsContent $block): string
