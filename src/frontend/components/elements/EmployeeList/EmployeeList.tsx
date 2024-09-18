e12f424882bb99ea3237f034efcc0208297cb24e
mitchellquitz <mitchell@allesonline.nl>
2024-09-18 21:45:02 +0200

    replace arbitrary tailwind class (and prettier)

--- src/frontend/components/elements/EmployeeList/EmployeeList.tsx
+++ src/frontend/components/elements/EmployeeList/EmployeeList.tsx
@@ -1,21 +1,19 @@
-import {EmployeeBlocks, EmployeeData} from "@/components/blocks/Employees";
+import { EmployeeBlocks, EmployeeData } from "@/components/blocks/Employees";
 import Image from "@/components/parts/Image";
 import ItemSubtitle, {
     ItemSubtitleColors,
     ItemSubtitleSizes,
 } from "@/components/parts/ItemSubtitle";
-import ItemTitle, {ListItemTitleColor, ListItemTitleSize} from "@/components/parts/ItemTitle";
-import Link, {LinkColors} from "@/components/parts/Link";
-import Text, {TextColors} from "@/components/parts/Text";
-import {generateHash} from "@/lib/helpers/generator.helper";
+import ItemTitle, { ListItemTitleColor, ListItemTitleSize } from "@/components/parts/ItemTitle";
+import Link, { LinkColors } from "@/components/parts/Link";
+import Text, { TextColors } from "@/components/parts/Text";
+import { generateHash } from "@/lib/helpers/generator.helper";
 
-
-export function EmployeeList({items}: { items: EmployeeBlocks }) {
+export function EmployeeList({ items }: { items: EmployeeBlocks }) {
     return (
-
         <div className="px-6 pb-20 pt-16 lg:px-8 lg:pb-28 lg:pt-24">
             <div className="relative">
-                <div className="grid max-w-lg- gap-5 lg:max-w-none md:grid-cols-1 lg:grid-cols-3">
+                <div className="max-w-lg- grid gap-5 md:grid-cols-1 lg:max-w-none lg:grid-cols-3">
                     {items.map((item) => (
                         <EmployeeCard
                             key={generateHash(item._identifier)}
@@ -28,20 +26,19 @@ export function EmployeeList({items}: { items: EmployeeBlocks }) {
     );
 }
 
-function EmployeeCard({image, name, jobTitle, description, email}: EmployeeData) {
+function EmployeeCard({ image, name, jobTitle, description, email }: EmployeeData) {
     return (
-        <div
-            className="flex flex-col md:flex-row lg:flex-col h-auto md:h-[244px] lg:h-auto overflow-hidden rounded-lg shadow-lg">
-            <div className="flex-shrink-0 w-full md:w-1/2 lg:w-full">
+        <div className="md:h-61 flex h-auto flex-col overflow-hidden rounded-lg shadow-lg md:flex-row lg:h-auto lg:flex-col">
+            <div className="w-full flex-shrink-0 md:w-1/2 lg:w-full">
                 <Image
                     image={image}
                     alt={name}
                     height={205}
-                    className="w-full h-52 md:h-full lg:h-52 object-cover"
+                    className="h-52 w-full object-cover md:h-full lg:h-52"
                 />
             </div>
-            <div className="flex flex-1 flex-col justify-between bg-teal-300 px-6 pt-6 pb-10">
-                <div className="flex-1 min-h-44">
+            <div className="flex flex-1 flex-col justify-between bg-teal-300 px-6 pb-10 pt-6">
+                <div className="min-h-44 flex-1">
                     <ItemTitle
                         title={name}
                         color={ListItemTitleColor.NEUTRAL}
