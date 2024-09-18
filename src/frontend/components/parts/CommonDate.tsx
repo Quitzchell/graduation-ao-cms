5af0a2916757a165833ef2617bfd0b6d3874b130
mitchellquitz <mitchell@allesonline.nl>
2024-09-18 14:57:51 +0200

    update CommonDate.tsx

--- src/frontend/components/parts/CommonDate.tsx
+++ src/frontend/components/parts/CommonDate.tsx
@@ -1,8 +1,18 @@
 import PropTypes from "prop-types";
 
 import Icon, { IconColors } from "@/components/parts/Icon";
+import { cn } from "@/lib/utils";
 
-function CommonDate({ date }) {
+export enum CommonDateColors {
+    BLUE = "blue",
+}
+
+const colorMap = {
+    [CommonDateColors.BLUE]: { text: "text-blue-75", icon: IconColors.BLUE },
+};
+function CommonDate({ date, color }) {
+    const { text, iconColor } = colorMap[color] || {};
+    const dateClass = cn(text);
     const dateFormatted = date.toLocaleString("nl-NL", {
         day: "numeric",
         month: "short",
@@ -12,18 +22,19 @@ function CommonDate({ date }) {
     return (
         <div className="flex items-start gap-x-2">
             <Icon
-                color={IconColors.BLUE}
+                color={iconColor}
                 name="calendar"
                 width={20}
                 height={20}
             />
-            <div className={"text-blue-75"}>{dateFormatted}</div>
+            <div className={dateClass}>{dateFormatted}</div>
         </div>
     );
 }
 
 CommonDate.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
+    color: PropTypes.oneOf(Object.values(CommonDateColors)),
 };
 
 export default CommonDate;
