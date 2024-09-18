c1d75a26596b4556540113485f63ab35185ec3bd
mitchellquitz <mitchell@allesonline.nl>
2024-09-18 16:31:26 +0200

    fix typo

--- src/frontend/app/(templates)/[...segments]/page.tsx
+++ src/frontend/app/(templates)/[...segments]/page.tsx
@@ -8,7 +8,7 @@ export default async function Page({ params }: { params: { segments: string[] } 
         <div>
             <h1>Page</h1>
             <div>{JSON.stringify(page)}</div>
-            {page?.blocks !== null && <Blocks blocks={page.blocks} />}]
+            {page?.blocks !== null && <Blocks blocks={page.blocks} />}
         </div>
     );
 }
