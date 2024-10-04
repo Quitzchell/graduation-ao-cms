109714cfe49f8ad7423a9c2f3bd7c8a17ae2fc01
mitchellquitz <mitchell@allesonline.nl>
2024-10-04 14:37:34 +0200

    update route for fetching-person information

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Actions\Person\FetchPersonAction;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('fetch-person/{uuid}', FetchPersonAction::class);
 
 Route::any('{all}', [\App\Http\Controllers\ContentController::class, 'getIndex'])
     ->where('all', '.*')
