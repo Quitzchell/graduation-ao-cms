b9d78a59408286368eaa014abf8b4b257fad65c1
mitchellquitz <mitchell@allesonline.nl>
2024-10-04 14:32:46 +0200

    rename fetchPage to fetchUtils, so it can store multiple fetch functions

---
+++ src/frontend/lib/fetchUtils.ts
@@ -0,0 +1,39 @@
+export const fetchPage = async (slug: string) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${slug}`, {
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            next: {
+                revalidate: process.env.NEXT_PRIVATE_DEBUG === "true" ? 0 : 60
+            }
+        });
+        return res.json();
+    } catch (error) {
+        // TODO error handling
+        console.log(error);
+    }
+
+    return null;
+};
+
+export const fetchPerson = async (uuid: string) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/fetch-person/${uuid}`, {
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            next: {
+                revalidate: process.env.NEXT_PRIVATE_DEBUG === "true" ? 0 : 60
+            }
+        });
+        return res.json()
+    } catch (error) {
+        // TODO error handling
+        console.log(error);
+    }
+
+    return null;
+}
