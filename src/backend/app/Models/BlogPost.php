e9694701e837ef68ffcb72f22698abb8d525fd9c
mitchellquitz <mitchell@allesonline.nl>
2024-10-06 20:03:13 +0200

    initial set up for BlogPosts

---
+++ src/backend/app/Models/BlogPost.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Eloquent;
+
+class BlogPost extends Eloquent
+{
+    protected $table = 'blog_posts';
+
+
+}
--- src/backend/database/migrations/2024_10_06_151616_create_blog_posts_table.php
+++ src/backend/database/migrations/2024_10_06_151616_create_blog_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('excerpt')->nullable();
             $table->foreignIdFor(Category::class);
+            $table->boolean('published')->default(true);
             $table->timestamps();
         });
     }
