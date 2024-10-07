8815c9de08bffefea5eb80be55c5d6bd52804b53
mitchellquitz <mitchell@allesonline.nl>
2024-10-07 22:42:03 +0200

    update styling

---
+++ src/frontend/blocks/blog/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Header({headerImage, headerTitle}) {
+    return (
+        <header className="h-80 md:h-100 lg:h-140 relative overflow-hidden w-screen">
+            <div className="h-full z-10 flex justify-center items-end">
+                <div className="bg-neutral-900/80 w-full py-3">
+                    <h1 className="text-neutral-0 w-3/4 mx-auto font-bold capitalize text-center text-3xl md:text-4xl">{headerTitle}</h1>
+                </div>
+            </div>
+            <Image
+                src={headerImage}
+                alt="header image"
+                quality={100}
+                fill
+                priority
+                sizes="w-screen h-fit"
+                className="object-cover md:object-top -z-10"
+            />
+        </header>
+    )
+}
--- src/frontend/blocks/common/Paragraph.tsx
+++ src/frontend/blocks/common/Paragraph.tsx
@@ -1,6 +1,6 @@
 export default function Paragraph({title, text}) {
     return (
-        <div className="flex flex-col py-2 gap-y-2">
+        <div className="flex flex-col py-2 gap-y-2 md:py-3">
             <h3 className="font-bold text-lg">{title}</h3>
             <div dangerouslySetInnerHTML={{__html: text}}/>
         </div>
