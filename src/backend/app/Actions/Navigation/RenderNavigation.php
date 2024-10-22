549f651d0362b1cbee21c565149a5b09cf3528df
mitchellquitz <mitchell@allesonline.nl>
2024-10-22 20:59:05 +0200

    optimize

--- src/backend/app/Actions/Navigation/RenderNavigation.php
+++ src/backend/app/Actions/Navigation/RenderNavigation.php
@@ -8,20 +8,18 @@ class RenderNavigation
 {
     public function __invoke(): JsonResponse
     {
-        // Get the ID of the main menu
         $mainMenuId = \ManagedContent::where('uri', 'Main Menu')->pluck('id')->first();
 
         // Query the pages associated with the main menu
         $pages = \Page::with('manager')->whereHas('manager', function ($query) use ($mainMenuId) {
             $query->where('parent_id', $mainMenuId);
-        })->get()->map(function ($page) {
-            return [
+        })->where('template_name', '!=', 'home')
+            ->get()
+            ->map(static fn($page) => [
                 'name' => $page->name,
                 'uri' => $page->manager->uri,
-            ];
-        });
+            ]);
 
-        // Return the pages as a JSON response
         return response()->json($pages);
     }
 }
