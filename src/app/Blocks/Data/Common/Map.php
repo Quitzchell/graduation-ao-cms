2d7965924b31ec1b89dc37ed85f10e1d0bee2582
mitchellquitz <mitchell@allesonline.nl>
2024-10-27 21:45:55 +0100

    pass google embedded map key

--- src/app/Blocks/Data/Common/Map.php
+++ src/app/Blocks/Data/Common/Map.php
@@ -11,11 +11,13 @@ class Map extends BaseBlock
     public string $text;
 
     public string $location;
+    public string $mapKey;
 
     public function __construct(\CmsContent $block)
     {
         $this->title = $block->content('title');
         $this->text = $block->content('text');
         $this->location = $block->content('location');
+        $this->mapKey = config('services.google_maps_key');
     }
 }
